66ca5199e8e9119ca15bea32fbc2e6032bbc668e
Bartosz Wojtaś <el.klides@op.pl>
2018-03-29 14:10:33 +0200

    Add all files

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+	'GOOGLE_CLIENT_ID':'67757529433-rk1eclovfr2lu1co50feosqdtc50te9u.apps.googleusercontent.com',
+	'GOOGLE_CLIENT_SECRET':'CeWgJjPV21Q3FGUHopvy3_h2',
+	'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+};
